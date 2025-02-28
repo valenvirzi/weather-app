21bcc5cd15f37653ed4a5e36cd4487a81527ab92
Valentin Virzi <v.virzi@hotmail.com>
2025-02-28 19:18:16 -0300

    display fix

--- src/components/HumidityChart/HumidityItem.tsx
+++ src/components/HumidityChart/HumidityItem.tsx
@@ -5,7 +5,7 @@ const HumidityItem: React.FC<ItemDetailProps> = ({ item }) => {
     <li
       className="relative flex max-w-7 flex-col items-center text-center"
       style={{
-        top: `${item.main.humidity >= 90 ? -(item.main.humidity - 90) : Math.max(0, Math.min(110, 1400 / item.main.humidity))}px`,
+        top: `${Math.round(item.main.humidity >= 90 ? -(item.main.humidity - 90) : Math.max(0, Math.min(110, 1400 / item.main.humidity)))}px`,
       }}
     >
       <span className="text-sm lg:text-base">{item.main.humidity}%</span>
