bd14dd6c54af689f6760b6958b3c01793589a1b9
Valentin Virzi <v.virzi@hotmail.com>
2025-03-07 19:28:35 -0300

    fix

--- src/components/WindChart/WindChart.tsx
+++ src/components/WindChart/WindChart.tsx
@@ -4,7 +4,7 @@ import WindItem from "./WindItem";
 const WindChart: React.FC = () => {
   const { weatherData } = useWeatherData();
   return (
-    <ul className="flex h-full w-full items-center gap-10 overflow-x-auto px-10">
+    <ul className="flex w-full items-center gap-10 overflow-x-auto px-10">
       {weatherData.forecast?.list.map((item) => {
         return <WindItem key={item.dt} item={item} />;
       })}
